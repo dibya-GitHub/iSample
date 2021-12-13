d5272d41fd02af227ac272ad55848c899972c877
LAPTOP-Q8FF4VD4\Dibya ranjan <softwaredibya@gmail.com>
2021-12-13 23:09:11 +0530

    file cleaning

--- src/app/folder/folder-routing.module.ts
+++ src/app/folder/folder-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
 import { FolderPage } from './folder.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: FolderPage
-  }
+    component: FolderPage,
+  },
 ];
 
 @NgModule({
